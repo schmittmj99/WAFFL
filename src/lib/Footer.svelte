b7770f22619c9e0d70cc4b2e31ababec02be6cdb
schmittmj99 <90440601+schmittmj99@users.noreply.github.com>
2021-09-20 00:16:27 -0500

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -32,7 +32,7 @@
 
 <style>
 	footer {
-		background-color: var(--fff); //background-color: var(--f8f8f8);
+		background-color: var(--f8f8f8);
 		width: 100%;
         display: block;
         position: absolute;
@@ -108,6 +108,9 @@
 	<br />
 	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
 	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
+	<!-- You can remove the donation link (although any donations to help
+	 maintain and enhance League Page would be greatly appreciated!) -->
+	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
 </footer>
 
 <footer class="{!outOfDate ? '' : 'invisible'}" bind:this={el}>
@@ -129,4 +132,7 @@
 	<br />
 	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
 	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
+	<!-- You can remove the donation link (although any donations to help
+	 maintain and enhance League Page would be greatly appreciated!) -->
+	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
 </footer>
